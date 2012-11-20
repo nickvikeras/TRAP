783549b4b2f8f6f24f44702268f628179e96fd0e
soule047 <soule047@umn.edu>
2012-11-19 22:52:13 -0600

    Adding Grant Rules

---
+++ src/main/edu/umn/se/trap/rule/grantrule/DodNoBreakfastRule.java
@@ -0,0 +1,115 @@
+/*
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License. 
+ */
+package edu.umn.se.trap.rule.grantrule;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+
+import edu.umn.se.trap.TrapException;
+import edu.umn.se.trap.form.Expense;
+import edu.umn.se.trap.form.ExpenseType;
+import edu.umn.se.trap.form.FormGrant;
+import edu.umn.se.trap.form.GrantSet;
+import edu.umn.se.trap.form.TrapForm;
+import edu.umn.se.trap.rule.AbstractRule;
+
+/**
+ * @author Mark
+ * 
+ */
+public class DodNoBreakfastRule extends AbstractRule
+{
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * edu.umn.se.trap.rule.AbstractRule#validateRule(edu.umn.se.trap.form.TrapForm
+     * )
+     */
+    @Override
+    public void validateRule(TrapForm form) throws TrapException
+    {
+
+        if (form != null)
+        {
+            checkExpenseGrants(form.getExpenses());
+
+        }
+        else
+        {
+            throw new TrapException("Invalid TrapForm object: form was null.");
+        }
+
+    }
+
+    /**
+     * @param expenses
+     * @throws TrapException
+     */
+    private void checkExpenseGrants(List<Expense> expenses)
+            throws TrapException
+    {
+
+        for (Expense expense : expenses)
+        {
+
+            if (expense.getType().equals(ExpenseType.BREAKFAST))
+
+            {
+                GrantSet grantSet = expense.getEligibleGrants();
+
+                if (grantSet == null)
+                {
+                    throw new TrapException(
+                            "Invalid TrapForm object: grantSet was null.");
+                }
+
+                Set<FormGrant> grants = grantSet.getGrants();
+
+                if (grants == null)
+                {
+                    throw new TrapException(
+                            "Invalid TrapForm object: grants was null.");
+                }
+
+                Iterator<FormGrant> grantIter = grants.iterator();
+
+                while (grantIter.hasNext())
+                {
+                    FormGrant grant = grantIter.next();
+
+                    if (StringUtils.equalsIgnoreCase(
+                            grant.getFundingOrganization(), "DOD"))
+                    {
+                        throw new TrapException(
+                                "The DOD grant will not cover breakfast costs.");
+                    }
+                }
+
+            }
+
+        }
+
+    }
+
+}
