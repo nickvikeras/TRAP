b812821654b8b8fd0d4910a6dac165b40ebeb1f7
soule047 <soule047@umn.edu>
2012-11-26 00:35:14 -0600

    Wrote main body of FormCheckerFactory.

--- src/main/edu/umn/se/trap/rule/FormCheckerFactory.java
+++ src/main/edu/umn/se/trap/rule/FormCheckerFactory.java
@@ -18,10 +18,35 @@
  */
 package edu.umn.se.trap.rule;
 
+import edu.umn.se.trap.rule.businessrule.BaggageCostLimitRule;
+import edu.umn.se.trap.rule.businessrule.ExportGrantCitizenRule;
+import edu.umn.se.trap.rule.businessrule.FileDeadlineRule;
+import edu.umn.se.trap.rule.businessrule.GrantAuthorizationRule;
+import edu.umn.se.trap.rule.businessrule.HotelOvernightRule;
+import edu.umn.se.trap.rule.businessrule.ItemizedCostsBetweenDatesRule;
+import edu.umn.se.trap.rule.businessrule.NumBaggageClaimsRule;
+import edu.umn.se.trap.rule.businessrule.PersonalAndRentalCarRule;
+import edu.umn.se.trap.rule.businessrule.TravelTypeMatchesGrantRule;
+import edu.umn.se.trap.rule.businessrule.USCarrierRule;
+import edu.umn.se.trap.rule.businessrule.VisaStatusRule;
+import edu.umn.se.trap.rule.grantrule.DodGrantHertzRule;
+import edu.umn.se.trap.rule.grantrule.DodNoBreakfastRule;
+import edu.umn.se.trap.rule.grantrule.DodNoForeignRule;
+import edu.umn.se.trap.rule.grantrule.ForeignGrantsForeignTravelOnlyRule;
+import edu.umn.se.trap.rule.grantrule.NihNoFoodRule;
+import edu.umn.se.trap.rule.grantrule.NihOnlyPublicTransitOrAirfareRule;
+import edu.umn.se.trap.rule.grantrule.NonDodGrantNationalRule;
+import edu.umn.se.trap.rule.grantrule.SponsoredNoAlcoholRule;
+import edu.umn.se.trap.rule.grantrule.SponsoredNoInternetRule;
 import edu.umn.se.trap.rule.wellformedrule.ArrivalAfterDepartureRule;
+import edu.umn.se.trap.rule.wellformedrule.RequiredFieldsRule;
+import edu.umn.se.trap.rule.wellformedrule.ValidDollarAmountRule;
 
 /**
  * @author nick
+ * @author Mark
+ * 
+ *         Creates the checkers for every rule.
  * 
  */
 public class FormCheckerFactory
@@ -29,21 +54,48 @@ public class FormCheckerFactory
     public static FormChecker createWellFormedChecker()
     {
         FormChecker checker = new FormChecker();
+
+        checker.addRule(new RequiredFieldsRule());
+        checker.addRule(new ValidDollarAmountRule());
         checker.addRule(new ArrivalAfterDepartureRule());
+
         return checker;
+
     }
 
     public static FormChecker createBusinessRuleChecker()
     {
         FormChecker checker = new FormChecker();
-        // add rules here with checker.addRule(rule);
+
+        checker.addRule(new BaggageCostLimitRule());
+        checker.addRule(new ExportGrantCitizenRule());
+        checker.addRule(new FileDeadlineRule());
+        checker.addRule(new GrantAuthorizationRule());
+        checker.addRule(new HotelOvernightRule());
+        checker.addRule(new ItemizedCostsBetweenDatesRule());
+        checker.addRule(new NumBaggageClaimsRule());
+        checker.addRule(new PersonalAndRentalCarRule());
+        checker.addRule(new TravelTypeMatchesGrantRule());
+        checker.addRule(new USCarrierRule());
+        checker.addRule(new VisaStatusRule());
+
         return checker;
     }
 
     public static FormChecker createGrantRuleChecker()
     {
         FormChecker checker = new FormChecker();
-        // add rules here with checker.addRule(rule);
+
+        checker.addRule(new DodGrantHertzRule());
+        checker.addRule(new DodNoBreakfastRule());
+        checker.addRule(new DodNoForeignRule());
+        checker.addRule(new ForeignGrantsForeignTravelOnlyRule());
+        checker.addRule(new NihNoFoodRule());
+        checker.addRule(new NihOnlyPublicTransitOrAirfareRule());
+        checker.addRule(new NonDodGrantNationalRule());
+        checker.addRule(new SponsoredNoAlcoholRule());
+        checker.addRule(new SponsoredNoInternetRule());
+
         return checker;
     }
 }
