2cca3f37081f0eb357c9dc3e5ebf187879b7df1f
soule047 <soule047@umn.edu>
2012-12-09 17:59:51 -0600

    Adding general coverage tests.

---
+++ src/test/system/edu/umn/se/trap/GeneralCoverageTests.java
@@ -0,0 +1,132 @@
+/*
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License. 
+ */
+package edu.umn.se.trap;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.umn.se.trap.util.TrapErrors;
+
+/**
+ * @author Mark
+ *
+ */
+public class GeneralCoverageTests extends AbstractSystemTest
+{
+
+    /* (non-Javadoc)
+     * @see edu.umn.se.trap.AbstractSystemTest#setUp()
+     */
+    @Before
+    public void setUp() throws Exception
+    {
+        super.setUp();
+    }
+
+    /* (non-Javadoc)
+     * @see edu.umn.se.trap.AbstractSystemTest#tearDown()
+     */
+    @After
+    public void tearDown() throws Exception
+    {
+        super.tearDown();
+    }
+    
+    @Test
+    public void testIncidentalCoverageAmountTooHigh() throws Exception
+    {
+        Map<String, String> input = getIncidentalInput();
+        Map<String, String> output = getIncidentalOutput();
+
+        // TODO
+        Printer.print(input, output);
+
+        try
+        {
+            SystemTestUtil.submitFormData(input, "desc", testProcessor, output);
+
+        }
+        catch (TrapException e)
+        {
+            assertEquals(TrapErrors.INCIDENTAL_AMOUNT_TOO_HIGH, e.getMessage());
+            return;
+        }
+    }
+    
+    @Test
+    public void testIncidentalCoverage() throws Exception
+    {
+        Map<String, String> input = getIncidentalInput();
+        Map<String, String> output = getIncidentalOutput();
+        
+        input.put("DAY1_INCIDENTAL_AMOUNT", "4.00");
+        
+        output.put("DAY1_TOTAL", "115.81");
+        output.put("GRANT1_AMOUNT_TO_CHARGE", "1050.24");
+        output.put("TOTAL_REIMBURSEMENT", "1050.24");
+        
+        try
+        {
+            SystemTestUtil.submitFormData(input, "desc", testProcessor, output);
+
+        }
+        catch (TrapException e)
+        {
+            fail("No exception should have been thrown: " + e.getMessage());
+        }
+
+        // TODO
+        Printer.print(input, output);
+    }
+    
+    @Test
+    public void testIncidentalCoverageCityOnly() throws Exception
+    {
+        Map<String, String> input = getIncidentalInput();
+        Map<String, String> output = getIncidentalOutput();
+        
+        input.put("DAY1_INCIDENTAL_AMOUNT", "4.00");
+        input.remove("DAY1_INCIDENTAL_STATE");
+        input.remove("DAY1_INCIDENTAL_COUNTRY");
+        
+        output.put("DAY1_TOTAL", "115.81");
+        output.put("GRANT1_AMOUNT_TO_CHARGE", "1050.24");
+        output.put("TOTAL_REIMBURSEMENT", "1050.24");
+        
+        // TODO
+        Printer.print(input, output);
+        
+        try
+        {
+            SystemTestUtil.submitFormData(input, "desc", testProcessor, output);
+
+        }
+        catch (TrapException e)
+        {
+            assertEquals(TrapErrors.INCIDENTAL_AMOUNT_GREATER_THAN_ZERO, e.getMessage());
+            return;
+        }
+    }
+
+}
